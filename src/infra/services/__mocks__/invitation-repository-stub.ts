7b48c6f850374bb1796df94c23d6976200c03c80
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-06-04 13:08:21 -0300

    test: implement invitation repository stub for testing

---
+++ src/infra/services/__mocks__/invitation-repository-stub.ts
@@ -0,0 +1,36 @@
+import {
+    IAddInvitationModel,
+    IInvite,
+    IInviteStatus,
+} from "@/domain/models/invite"
+import { IInvitationRepository } from "@/domain/repositories-interfaces/invitation-repository"
+import { makeFakeInvite } from "./testing-factories"
+
+export class InvitationRepositoryStub implements IInvitationRepository {
+    async save(invite: IAddInvitationModel): Promise<string | null> {
+        const insertedId = "123"
+        return insertedId
+    }
+    async remove(id: string): Promise<boolean> {
+        return true
+    }
+    async checkById(id: string): Promise<boolean> {
+        return true
+    }
+    async updateStatus(
+        inviteId: string,
+        status: IInviteStatus
+    ): Promise<boolean> {
+        return true
+    }
+    async get(id: string): Promise<IInvite> {
+        return makeFakeInvite({ id })
+    }
+    async listUserInvitations(userId: string): Promise<IInvite[]> {
+        return [
+            makeFakeInvite({ id: "1", userId }),
+            makeFakeInvite({ id: "2", userId }),
+            makeFakeInvite({ id: "3", userId }),
+        ]
+    }
+}
